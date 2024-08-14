ee0aa95cc7673d8f26014b5565ecfa2c053b41f0
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-14 15:45:56 +0300

    Styling the hero section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -5,12 +5,18 @@ import heroImage from '../assets/images/Hero.png';
 
 const HeroSection: React.FC = () => {
   return (
-    <div className="relative h-screen bg-cover bg-center" style={{ backgroundImage: `url(${heroImage})` }}>
+    <div
+      className="relative h-screen bg-cover bg-center"
+      style={{ backgroundImage: `url(${heroImage})` }}
+    >
       <Navbar />
-      <div className="flex flex-col items-center justify-center h-full text-center text-[#123962]">
-        <h1 className="text-6xl font-bold">SmartCampus</h1>
-        <hr className="border-t-4 border-[#123962] w-32 my-4" />
-        <Button>Apply Now</Button>
+      <div className="flex flex-col items-center justify-end h-full text-center text-[#123962] pb-16">
+        <div className="flex items-center justify-center w-full px-8">
+          <hr className="border-t-[10px] border-[#123962] flex-grow opacity-85 transform -translate-x-8" style={{ height: '10px' }} />
+          <h1 className="text-7xl font-bold opacity-85">SmartCampus</h1>
+          <hr className="border-t-[10px] border-[#123962] flex-grow opacity-85 transform translate-x-8" style={{ height: '10px' }} />
+        </div>
+        <Button className="mt-8 opacity-85">Apply Now</Button>
       </div>
     </div>
   );
