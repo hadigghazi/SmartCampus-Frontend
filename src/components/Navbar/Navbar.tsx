6d491738bf9dc5739a81f8ce5b4679ea63a804d8
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-14 18:41:59 +0300

    Fixing the welcome section

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,13 +5,13 @@ import styles from './Navbar.module.css';
 const Navbar: React.FC = () => {
   return (
     <nav className={styles.nav}>
-      <img src={NavbarLogo} alt="header_logo" className={`${styles.logo} ${styles.logo.md}`} />
-      <ul className={`${styles.menu} ${styles.menu.md}`}>
-        <li className={`${styles.menuItem} ${styles.menuItem.md}`}><a href="#admissions">Admissions</a></li>
-        <li className={`${styles.menuItem} ${styles.menuItem.md}`}><a href="#academics">Academics</a></li>
-        <li className={`${styles.menuItem} ${styles.menuItem.md}`}><a href="#campuses">Campuses</a></li>
-        <li className={`${styles.menuItem} ${styles.menuItem.md}`}><a href="#library">Library</a></li>
-        <li className={`${styles.menuItem} ${styles.menuItem.md}`}><a href="#portal">Portal</a></li>
+      <img src={NavbarLogo} alt="header_logo" className={`${styles.logo}`} />
+      <ul className={`${styles.menu}`}>
+        <li className={`${styles.menuItem}`}><a href="#admissions">Admissions</a></li>
+        <li className={`${styles.menuItem}`}><a href="#academics">Academics</a></li>
+        <li className={`${styles.menuItem}`}><a href="#campuses">Campuses</a></li>
+        <li className={`${styles.menuItem}`}><a href="#library">Library</a></li>
+        <li className={`${styles.menuItem}`}><a href="#portal">Portal</a></li>
       </ul>
     </nav>
   );
