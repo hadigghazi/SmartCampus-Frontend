4ef08d0dceac80357455790645fb034d8a82a9f3
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-19 08:49:20 +0300

    Adding the instructor

--- src/components/Instructor/Environment.tsx
+++ src/components/Instructor/Environment.tsx
@@ -12,6 +12,12 @@ const Environment: React.FC = () => {
         position={[0, -8, 0]}
         rotation={[0, Math.PI, 0]}
       />
+      <Gltf
+        src="/models/avatar.glb"
+        scale={(7)}
+        position={[-11, -7.95, -14]}
+        rotation={[0, 0.5, 0]}
+      />
     </Canvas>
   );
 }
