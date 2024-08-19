404c653298445e8d6110e867d08913c7e33a13a0
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-19 09:19:48 +0300

    Adding Instructor component

---
+++ public/models/Instructor.tsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useRef } from "react";
+import { useGLTF, useAnimations } from "@react-three/drei";
+import { Group, SkinnedMesh, Object3D, Material } from "three";
+import modelPath from "./avatar.glb";
+
+type GLTFResult = {
+  nodes: {
+    avaturn_body: SkinnedMesh;
+    avaturn_hair_0: SkinnedMesh;
+    avaturn_look_0: SkinnedMesh;
+    avaturn_shoes_0: SkinnedMesh;
+    Hips: Object3D;
+  };
+  materials: {
+    avaturn_body_material: Material;
+    avaturn_hair_0_material: Material;
+    avaturn_look_0_material: Material;
+    avaturn_shoes_0_material: Material;
+  };
+};
+
+export function Emilian(props: JSX.IntrinsicElements["group"]) {
+  const group = useRef<Group>(null);
+  const { nodes, materials, animations } = useGLTF(modelPath) as unknown as GLTFResult;
+  const { actions, mixer } = useAnimations(animations, group);
+
+  useEffect(() => {
+    const idleAction = mixer.clipAction(
+      animations.find((a) => a.name === "IdleV4.2(maya_head)")!
+    );
+    idleAction.play();
+  }, [mixer, animations]);
+
+  return (
+    <group ref={group} {...props} dispose={null}>
+      <group name="Scene">
+        <group name="hadi-ghazi">
+          <skinnedMesh
+            name="avaturn_body"
+            geometry={nodes.avaturn_body.geometry}
+            material={materials.avaturn_body_material}
+            skeleton={nodes.avaturn_body.skeleton}
+          />
+          <skinnedMesh
+            name="avaturn_hair_0"
+            geometry={nodes.avaturn_hair_0.geometry}
+            material={materials.avaturn_hair_0_material}
+            skeleton={nodes.avaturn_hair_0.skeleton}
+          />
+          <skinnedMesh
+            name="avaturn_look_0"
+            geometry={nodes.avaturn_look_0.geometry}
+            material={materials.avaturn_look_0_material}
+            skeleton={nodes.avaturn_look_0.skeleton}
+          />
+          <skinnedMesh
+            name="avaturn_shoes_0"
+            geometry={nodes.avaturn_shoes_0.geometry}
+            material={materials.avaturn_shoes_0_material}
+            skeleton={nodes.avaturn_shoes_0.skeleton}
+          />
+          <primitive object={nodes.Hips} />
+        </group>
+      </group>
+    </group>
+  );
+}
+
+useGLTF.preload(modelPath);
--- src/components/Instructor/Experience.tsx
+++ src/components/Instructor/Experience.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Gltf, Environment } from '@react-three/drei';
+import Instructor from '../../../public/models/Instructor';
 
 const Experience: React.FC = () => {
   return (
@@ -8,17 +9,18 @@ const Experience: React.FC = () => {
       <OrbitControls />
       <Environment preset="sunset" />
       <ambientLight intensity={0.8} color={"pink"} />
+      
       <Gltf
         src="/models/classroom.glb"
         position={[0, -8, 0]}
         rotation={[0, Math.PI, 0]}
       />
-      <Gltf
+      <Instructor
         src="/models/avatar.glb"
         scale={(7)}
         position={[-11, -7.95, -14]}
         rotation={[0, 0.5, 0]}
-      />
+       />
     </Canvas>
   );
 }
