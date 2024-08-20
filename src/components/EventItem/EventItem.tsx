bb778f487b383fcdd65c3a73b9e1d8d0d97773da
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-20 13:38:11 +0300

    Adding important dates item

--- src/components/EventItem/EventItem.tsx
+++ src/components/EventItem/EventItem.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import styles from './EventItem.module.css';
 
-interface EventItemProps {
-    date: string;
-    month: string;
-    description: string;
+type EventItemProps = {
+  date: string;
+  month: string;
+  description: string;
 }
 
 const EventItem: React.FC<EventItemProps> = ({ date, month, description }) => {
-    return (
-        <div className={styles.eventItem}>
-            <div className={styles.dateBox}>
-                <span className={styles.date}>{date}</span>
-                <span className={styles.month}>{month}</span>
-            </div>
-            <div className={styles.description}>
-                {description}
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className={styles.eventItem}>
+      <div className={styles.dateBox}>
+        <span className={styles.date}>{date}</span>
+        <span className={styles.month}>{month}</span>
+      </div>
+      <div className={styles.description}>
+        {description}
+      </div>
+    </div>
+  );
+};
 
 export default EventItem;
