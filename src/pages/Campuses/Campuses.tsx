09e6e5639959ad3ac2a20133969ef12d2b9dc15a
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-20 18:32:19 +0300

    Styling the campuses

--- src/pages/Campuses/Campuses.tsx
+++ src/pages/Campuses/Campuses.tsx
@@ -3,8 +3,15 @@ import styles from './Campuses.module.css';
 import headerImage from '../../assets/images/Pages_header.jpg';
 import Navbar from '../../components/Navbar/Navbar';
 import Footer from '../../components/Footer/Footer';
+import CampusCard from '../../components/CampusCard/CampusCard';
+import { useGetCampusesQuery } from '../../features/api/campusesApi';
 
 const Campuses: React.FC = () => {
+  const { data: campuses, error, isLoading } = useGetCampusesQuery();
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error loading campuses</p>;
+
   return (
     <div className={styles.layout}>
       <Navbar />
@@ -13,7 +20,18 @@ const Campuses: React.FC = () => {
         <h1 className={styles.pageTitle}>Campuses</h1>
       </div>
       <div className={styles.content}>
-        
+      <h2 className={styles.headingSecondary}>- Our Campuses</h2>
+      <h1 className={styles.headingPrimary}>Explore Campuses</h1>
+      <div className={styles.campuses}>
+        {campuses?.map((campus) => (
+          <CampusCard
+            key={campus.id}
+            id={campus.id}
+            name={campus.name}
+            image={headerImage} 
+          />
+        ))}
+        </div>
       </div>
       <Footer />
     </div>
