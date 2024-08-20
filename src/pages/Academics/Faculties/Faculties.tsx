222959c4a0c976ebe59ab97b7af9df9850b852a6
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-20 15:19:19 +0300

    creating the faculties component

--- src/pages/Academics/Faculties/Faculties.tsx
+++ src/pages/Academics/Faculties/Faculties.tsx
@@ -1,9 +1,27 @@
-import AcademicsLayout from "../AcademicsLayout"
+import AcademicsLayout from "../AcademicsLayout";
+import styles from './Faculties.module.css';
+import FacultiesCard from '../../../components/FacultiesCard/FacultiesCard';
+import { useGetFacultiesQuery } from '../../../features/api/facultiesApi';
 
 const Faculties = () => {
-  return (
-    <AcademicsLayout title="Faculties">Faculties</AcademicsLayout>
-  )
-}
+  const { data: faculties, error, isLoading } = useGetFacultiesQuery();
 
-export default Faculties
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error loading faculties</p>;
+
+  return (
+    <AcademicsLayout title="Faculties">
+      <div className={styles.container}>
+        <h2 className={styles.headingSecondary}>- Our Faculties</h2>
+        <h1 className={styles.headingPrimary}>Explore Faculties</h1>
+        <div className={styles.grid}>
+          {faculties?.map((faculty) => (
+            <FacultiesCard key={faculty.id} faculty={faculty} />
+          ))}
+        </div>
+      </div>
+    </AcademicsLayout>
+  );
+};
+
+export default Faculties;
