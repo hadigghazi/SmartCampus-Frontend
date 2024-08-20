4485824dbd1330a93e3c671c341146e6d0065ac9
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-20 11:40:36 +0300

    Styling news page

--- src/pages/NewsDetailPage/NewsDetailPage.tsx
+++ src/pages/NewsDetailPage/NewsDetailPage.tsx
@@ -1,11 +1,11 @@
-// src/pages/NewsDetailPage/NewsDetailPage.tsx
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useGetNewsByIdQuery } from '../../features/api/newsApi';
-import { News } from '../../features/api/types'; // Import the News type
+import styles from './NewsDetailPage.module.css'; 
+import AdmissionsLayout from '../Admissions/AdmissionsLayout';
 
 const NewsDetailPage: React.FC = () => {
-  const { id } = useParams<{ id: string }>(); // Get the ID from URL params
+  const { id } = useParams<{ id: string }>(); 
   const { data: news, error, isLoading } = useGetNewsByIdQuery(Number(id));
 
   if (isLoading) return <p>Loading...</p>;
@@ -14,11 +14,18 @@ const NewsDetailPage: React.FC = () => {
   if (!news) return <p>No news found</p>;
 
   return (
-    <div>
-      <h1>{news.title}</h1>
-      <p>{news.published_date}</p>
-      <p>{news.content}</p>
+   <AdmissionsLayout title={news.category}>
+    <div className={styles.container}>
+      <div className={styles.imageContainer}>
+        
+      </div>
+      <h1 className={styles.headingPrimary}>{news.published_date}</h1>
+      <h2 className={styles.headingSecondary}>{news.title}</h2>
+      <div className={styles.content}>
+        <p>{news.content}</p>
+      </div>
     </div>
+    </AdmissionsLayout>
   );
 };
 
