22ed1b1bbbfe9a9b0751879048114ad45db908e0
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-20 15:41:16 +0300

    Styling the majors page

--- src/pages/Academics/Majors/Majors.tsx
+++ src/pages/Academics/Majors/Majors.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import { useGetMajorsQuery } from '../../../features/api/majorsApi';
-import MajorCard from '../../../components/MajorCard/MajorCard';
+import AcademicsLayout from "../AcademicsLayout";
 import styles from './Majors.module.css';
+import MajorCard from '../../../components/MajorCard/MajorCard';
+import { useGetMajorsQuery } from '../../../features/api/majorsApi';
 
 const Majors: React.FC = () => {
   const { data: majors, error, isLoading } = useGetMajorsQuery();
 
-  if (isLoading) return <p>Loading majors...</p>;
+  if (isLoading) return <p>Loading...</p>;
   if (error) return <p>Error loading majors</p>;
 
   return (
-    <div className={styles.container}>
-      <h2 className={styles.headingSecondary}>- Our Majors</h2>
-      <h1 className={styles.headingPrimary}>Explore Our Majors</h1>
-      <div className={styles.grid}>
-        {majors?.map((major) => (
-          <MajorCard key={major.id} major={major} />
-        ))}
+    <AcademicsLayout title="Majors">
+      <div className={styles.container}>
+        <h2 className={styles.headingSecondary}>- Our Majors</h2>
+        <h1 className={styles.headingPrimary}>Explore Majors</h1>
+        <div className={styles.grid}>
+          {majors?.map((major) => (
+            <MajorCard key={major.id} major={major} />
+          ))}
+        </div>
       </div>
-    </div>
+    </AcademicsLayout>
   );
 };
 
