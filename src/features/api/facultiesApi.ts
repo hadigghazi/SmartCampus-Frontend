5f41ca33f9a5abcfcceebdfb82c280de55b8603a
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-20 12:59:10 +0300

    Add all crud operations to faculties api

--- src/features/api/facultiesApi.ts
+++ src/features/api/facultiesApi.ts
@@ -7,16 +7,45 @@ type Faculty = {
   created_at: string;
   updated_at: string;
   deleted_at: string | null;
-}
+};
 
 export const facultiesApi = createApi({
   reducerPath: 'facultiesApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8000/api' }), 
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8000/api' }),
   endpoints: (builder) => ({
     getFaculties: builder.query<Faculty[], void>({
       query: () => 'faculties',
     }),
+    getFacultyById: builder.query<Faculty, number>({
+      query: (id) => `faculties/${id}`,
+    }),
+    createFaculty: builder.mutation<Faculty, Partial<Faculty>>({
+      query: (newFaculty) => ({
+        url: 'faculties',
+        method: 'POST',
+        body: newFaculty,
+      }),
+    }),
+    updateFaculty: builder.mutation<Faculty, { id: number; updatedFaculty: Partial<Faculty> }>({
+      query: ({ id, updatedFaculty }) => ({
+        url: `faculties/${id}`,
+        method: 'PUT',
+        body: updatedFaculty,
+      }),
+    }),
+    deleteFaculty: builder.mutation<void, number>({
+      query: (id) => ({
+        url: `faculties/${id}`,
+        method: 'DELETE',
+      }),
+    }),
   }),
 });
 
-export const { useGetFacultiesQuery } = facultiesApi;
+export const {
+  useGetFacultiesQuery,
+  useGetFacultyByIdQuery,
+  useCreateFacultyMutation,
+  useUpdateFacultyMutation,
+  useDeleteFacultyMutation,
+} = facultiesApi;
