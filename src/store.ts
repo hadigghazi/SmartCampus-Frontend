8a12866708651499a5068ac9706fb99edbc7478c
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-20 10:34:39 +0300

    Updating the store

--- src/store.ts
+++ src/store.ts
@@ -1,14 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './features/auth/authSlice';
 import { authApi } from './features/api/authApi';
+import { newsApi } from './features/api/newsApi'; 
 
 export const store = configureStore({
   reducer: {
     [authApi.reducerPath]: authApi.reducer,
+    [newsApi.reducerPath]: newsApi.reducer,
     auth: authReducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(authApi.middleware),
+    getDefaultMiddleware().concat(authApi.middleware).concat(newsApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
