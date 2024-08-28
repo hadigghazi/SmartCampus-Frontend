61cea6bb99ddaffeaac713272cd4a165cf2087e7
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-28 10:40:50 +0300

    Adding grade slice

---
+++ src/features/api/gradesApi.ts
@@ -0,0 +1,45 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { Grade } from './types';
+
+const apiUrl = import.meta.env.VITE_BASE_URL;
+
+export const gradesApi = createApi({
+  reducerPath: 'gradesApi',
+  baseQuery: fetchBaseQuery({ baseUrl: apiUrl }),
+  endpoints: (builder) => ({
+    getGrades: builder.query<Grade[], void>({
+      query: () => '/grades',
+    }),
+    getGradeById: builder.query<Grade, number>({
+      query: (id) => `/grades/${id}`,
+    }),
+    createGrade: builder.mutation<Grade, Partial<Grade>>({
+      query: (grade) => ({
+        url: '/grades',
+        method: 'POST',
+        body: grade,
+      }),
+    }),
+    updateGrade: builder.mutation<void, Grade>({
+      query: (grade) => ({
+        url: `/grades/${grade.id}`,
+        method: 'PUT',
+        body: grade,
+      }),
+    }),
+    deleteGrade: builder.mutation<void, number>({
+      query: (id) => ({
+        url: `/grades/${id}`,
+        method: 'DELETE',
+      }),
+    }),
+  }),
+});
+
+export const {
+  useGetGradesQuery,
+  useGetGradeByIdQuery,
+  useCreateGradeMutation,
+  useUpdateGradeMutation,
+  useDeleteGradeMutation,
+} = gradesApi;
