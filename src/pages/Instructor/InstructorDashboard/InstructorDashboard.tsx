418762d1bd17efcf9e541a05a0e21486b9490031
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-29 14:23:06 +0300

    Finishing the instructor dashboard

--- src/pages/Instructor/InstructorDashboard/InstructorDashboard.tsx
+++ src/pages/Instructor/InstructorDashboard/InstructorDashboard.tsx
@@ -8,6 +8,7 @@ import { useGetAnnouncementsQuery } from '../../../features/api/announcementsApi
 import { useGetUserQuery } from '../../../features/api/authApi';
 import { useGetCurrentSemesterQuery } from '../../../features/api/semestersApi';
 import { useGetCoursesAssignedToInstructorQuery } from '../../../features/api/coursesApi';
+import { useGetExamInstructorDetailsQuery } from '../../../features/api/examsApi'; 
 import styles from './InstructorDashboard.module.css';
 
 const InstructorDashboard: React.FC = () => {
@@ -16,6 +17,7 @@ const InstructorDashboard: React.FC = () => {
   const { data: announcements } = useGetAnnouncementsQuery();
   const { data: coursesAssigned } = useGetCoursesAssignedToInstructorQuery(instructor?.id);
   const { data: currentSemester } = useGetCurrentSemesterQuery();
+  const { data: exams } = useGetExamInstructorDetailsQuery(instructor?.id); 
 
   const [currentPage, setCurrentPage] = useState(1);
   const announcementsPerPage = 6;
@@ -44,13 +46,23 @@ const InstructorDashboard: React.FC = () => {
   const courseColumns = [
     { header: 'Semester', accessor: 'semester_name' },
     { header: 'Code', accessor: 'course_code' },
-    { header: 'Course', accessor: 'course_name' },
+    { header: 'Course Name', accessor: 'course_name' },
     { header: 'Credits', accessor: 'credits' },
     { header: 'Room', accessor: (course) => `${course.campus_name}, ${course.room}` },
     { header: 'From', accessor: 'from_date' },
     { header: 'To', accessor: 'to_date' },
     { header: 'Schedule', accessor: 'schedule' },
     { header: 'Students', accessor: (course) => `${course.number_of_students}/${course.available_seats}` }
+  ];
+
+  const examColumns = [
+    { header: 'Semester', accessor: 'semester_name' },
+    { header: 'Code', accessor: 'course_code' },
+    { header: 'Course Name', accessor: 'course_name' },
+    { header: 'Date', accessor: 'date' },
+    { header: 'Time', accessor: 'time' },
+    { header: 'Duration', accessor: 'duration' },
+    { header: 'Room', accessor: (exam) => `${exam.campus_name}, ${exam.block_name}, ${exam.room_number}` }
   ];
 
   return (
@@ -87,6 +99,13 @@ const InstructorDashboard: React.FC = () => {
           <Table columns={courseColumns} data={filteredCoursesAssigned} />
         ) : (
           <p>Loading assigned courses...</p>
+        )}
+
+        <h2 className={styles.headingPrimary}>Exams</h2>
+        {exams ? (
+          <Table columns={examColumns} data={exams} />
+        ) : (
+          <p>Loading exam details...</p>
         )}
       </div>
     </div>
