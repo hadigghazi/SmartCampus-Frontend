9f936c10540500e273260e198764db6e0be446ce
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-31 09:27:30 +0300

    Adding course name and code to the details of student

--- src/pages/Student/CourseDetails/CourseDetails.tsx
+++ src/pages/Student/CourseDetails/CourseDetails.tsx
@@ -5,14 +5,23 @@ import { useFetchAssignmentsByInstructorQuery } from '../../../features/api/assi
 import MaterialsList from '../../../components/MaterialsList/MaterialsList';
 import AssignmentsList from '../../../components/AssignmentsList/AssignmentsList';
 import styles from './CourseDetails.module.css';
+import { useGetCourseDetailsByInstructorIdQuery } from '../../../features/api/coursesApi';
 
 const StudentCourseDetailsPage: React.FC = () => {
   const { courseInstructorId } = useParams<{ courseInstructorId: string }>();
   const { data: materials } = useFetchCourseMaterialsByInstructorQuery(Number(courseInstructorId));
   const { data: assignments } = useFetchAssignmentsByInstructorQuery(Number(courseInstructorId));
+  const { data: courseDetails, isLoading: isCourseLoading } = useGetCourseDetailsByInstructorIdQuery(Number(courseInstructorId));
 
   return (
     <div className={styles.container}>
+      {isCourseLoading ? (
+        <p>Loading course details...</p>
+      ) : (
+        <>
+          <h1 className={styles.headingPrimary}>{courseDetails?.course_name} ({courseDetails?.course_code})</h1>
+        </>
+      )}
       <MaterialsList
         materials={materials}
       />
