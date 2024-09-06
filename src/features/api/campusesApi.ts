a65b9bb91a484cb1d9823b6b0d97109612d017f8
Hadi Ghazi <hadigghazi@gmail.com>
2024-09-06 10:24:16 +0300

    Adding base query to campuses

--- src/features/api/campusesApi.ts
+++ src/features/api/campusesApi.ts
@@ -1,10 +1,10 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi } from '@reduxjs/toolkit/query/react';
 import { Campus, Faculty } from './types';
-const apiUrl = import.meta.env.VITE_BASE_URL;
+import { baseQuery } from './baseQuery';
 
 export const campusesApi = createApi({
   reducerPath: 'campusesApi',
-  baseQuery: fetchBaseQuery({ baseUrl: apiUrl }), 
+  baseQuery, 
   endpoints: (builder) => ({
     getCampuses: builder.query<Campus[], void>({
       query: () => '/campuses',
