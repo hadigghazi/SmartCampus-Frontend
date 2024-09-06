17ca017d90c44116222cc6b9fef9e9ad368eef48
Hadi Ghazi <hadigghazi@gmail.com>
2024-09-06 10:23:38 +0300

    Adding base query to centers

--- src/features/api/centersApi.ts
+++ src/features/api/centersApi.ts
@@ -1,10 +1,10 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi } from '@reduxjs/toolkit/query/react';
 import { Center } from './types';
-const apiUrl = import.meta.env.VITE_BASE_URL;
+import { baseQuery } from './baseQuery';
 
 export const centersApi = createApi({
   reducerPath: 'centersApi',
-  baseQuery: fetchBaseQuery({ baseUrl: apiUrl }), 
+  baseQuery, 
   endpoints: (builder) => ({
     getCenters: builder.query<Center[], void>({
       query: () => 'centers',
