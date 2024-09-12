aebd761a1473933c67ed4e967d912e83adf096b6
Hadi Ghazi <hadigghazi@gmail.com>
2024-09-12 20:26:54 +0300

    Addig course evaluations api

---
+++ src/features/api/courseEvaluationsApi.ts
@@ -0,0 +1,43 @@
+import { createApi } from '@reduxjs/toolkit/query/react';
+import { baseQuery } from './baseQuery';
+
+export const courseEvaluationsApi = createApi({
+  reducerPath: 'courseEvaluationsApi',
+  baseQuery,
+  endpoints: (builder) => ({
+    getAllCourseEvaluations: builder.query({
+      query: () => '/course-evaluations',
+    }),
+    getCourseEvaluationById: builder.query({
+      query: (id) => `/course-evaluations/${id}`,
+    }),
+    createCourseEvaluation: builder.mutation({
+      query: (newEvaluation) => ({
+        url: '/course-evaluations',
+        method: 'POST',
+        body: newEvaluation,
+      }),
+    }),
+    updateCourseEvaluation: builder.mutation({
+      query: ({ id, updatedEvaluation }) => ({
+        url: `/course-evaluations/${id}`,
+        method: 'PUT',
+        body: updatedEvaluation,
+      }),
+    }),
+    deleteCourseEvaluation: builder.mutation({
+      query: (id) => ({
+        url: `/course-evaluations/${id}`,
+        method: 'DELETE',
+      }),
+    }),
+  }),
+});
+
+export const {
+  useGetAllCourseEvaluationsQuery,
+  useGetCourseEvaluationByIdQuery,
+  useCreateCourseEvaluationMutation,
+  useUpdateCourseEvaluationMutation,
+  useDeleteCourseEvaluationMutation,
+} = courseEvaluationsApi;
