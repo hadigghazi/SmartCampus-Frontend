40726f06d682439351800299b0e43864aaf4e282
Hadi Ghazi <hadigghazi@gmail.com>
2024-09-21 00:55:22 +0300

    Fixing configurations

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import checker from 'vite-plugin-checker';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    checker({
+      typescript: {
+        tsconfigPath: './tsconfig.json',
+        async: true,
+      },
+    }),
+  ],
   assetsInclude: ["**/*.glb"],
   server: {
     host: true, 
